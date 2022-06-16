aef9cc5b4cf1150c2967cdac731524d0923a13f3
Mateo <Mateo429>
2022-06-15 21:43:49 -0500

    Se añade el cálculo del valor esperado de x²

--- qho.cpp
+++ qho.cpp
@@ -9,12 +9,14 @@ double action_change(const std::vector<double> X, const std::vector<double> X_ol
 
 int main(int argc, char **argv){
 
-  double m = 1;
+  double m = 2;
   double omega = 1;
   double epsilon = std::atof(argv[1]);
   int T = std::atoi(argv[2]);
   double N = std::atoi(argv[3]);
   int seed = 1;
+  double counter = 0;
+  double X_sq=0;
   
   std::mt19937 gen(seed);
   std::uniform_real_distribution<double> dis(-0.5,0.5);
@@ -22,11 +24,10 @@ int main(int argc, char **argv){
   double ratio = 0;
   double DS = 0;
   double u = 0;
+  int gap = 10;
 
   std::vector<double> X;
   X.resize(T);
-   std::vector<double> X_sq;
-  X_sq.resize(T);
   std::vector<double> X_old;
   X_old.resize(T);
   std::vector<double> X_new;
@@ -39,13 +40,12 @@ int main(int argc, char **argv){
   for(int ii=0; ii<T; ++ii){
     ++seed;
     X[ii] = dis(gen);
-    X_sq[ii] = X[ii]*X[ii];
     X_old[ii]=0;
     X_new[ii]=0;
     corr_data[ii]=0;
   }
 
-  for (int ii =0; ii<burn_in_st; ++ii){
+  for (int ii =0; ii<N; ++ii){
     for (int ii = 0; ii<T; ++ii){
       t = int(T*P(gen));
       X_old[t] = X[t];
@@ -56,11 +56,25 @@ int main(int argc, char **argv){
       if (u<ratio){
 	X[t] = X_new[t];
       }
-      else { X[t]=X_old[t;]}
-    }
+      else { X[t]=X_old[t];}
     }
 
+    if (ii%gap==0){
+
+      ++counter;
+      for (int ii=0; ii<T; ++ii){
+      X_sq += X[ii]*X[ii];
+      corr_data[ii] = X[ii]*X[ii-5];
+
+      }}
+
+    
+    
+  }
+
   
+  double mean_sq= X_sq/(2*T*counter);
+  std::cout<<"The expected value of x² is "<<mean_sq<<std::endl;
 
   
   
@@ -75,8 +89,8 @@ double action_change(const std::vector<double> X, const std::vector<double> X_ol
   DS =0.5*m*(std::pow(X[t+1]-X_new[t],2)-std::pow(X[t+1]-X_old[t],2) + 0.25*omega*omega*(std::pow(X[t+1]+X_new[t],2)
 											-std::pow(X[t+1]+X_old[t],2)));}
 
-  else{
+   else{
   DS =0.5*m*(std::pow(X[0]-X_new[t],2)-std::pow(X[0]-X_old[t],2) + 0.25*omega*omega*(std::pow(X[0]+X_new[t],2)
-											-std::pow(X[0]+X_old[t],2)));}
+  -std::pow(X[0]+X_old[t],2)));}
   return DS;
 }
